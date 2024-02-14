686a6db7511b37da1668d138b809a9c621606fe0
jeff-mz <jafarmirzapoor11@gmail.com>
2024-02-14 17:08:57 +0330

    style improved

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from "./assets/components/Footer";
 
 function App() {
   return (
-    <main className="w-full  bg-dark-primary m-auto py-1 px-4 mobile:px-8 mobile:py-4 flex flex-col	 justify-between">
+    <main className="w-full h-screen bg-dark-primary m-auto py-1 px-4 mobile:px-8 mobile:py-4 flex flex-col	 justify-between">
       <Navigation />
       <About />
       <Footer />
